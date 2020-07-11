2c5094530deb88aad3c726b145893a4191a4e5a2
Samson Adegboye <adegboye72@gmail.com>
2020-07-11 04:16:01 +0100

    same as previous

--- src/app/contact/contact-submit.php
+++ src/app/contact/contact-submit.php
@@ -1,32 +1,34 @@
 
 <?php
   if(!empty($_POST['contactForm'])){
-    echo 'contact form not empty';
-  }
-
-  function sanitizeEmail($field){
-    $field= filter_var($field, FILTER_SANITIZE_EMAIL);
-    
-    if(filter_var($field, FILTER_VALIDATE_EMAIL)){
-      return true;
-    }else{
-      return false;
+    function sanitizeEmail($field){
+      $field= filter_var($field, FILTER_SANITIZE_EMAIL);
+      
+      if(filter_var($field, FILTER_VALIDATE_EMAIL)){
+        return true;
+      }else{
+        return false;
+      }
     }
+  
+    $to= 'adegboye72@gmail.com';
+    $subject= 'Contact Us Form';
+    $message= 'message sent using php mail function.';
+    $headers= 'From: Contact Us @ laurel-ng.herokuapp.com';
+  
+    // check if email address is valid or not.
+    $secureCheck= sanitizeEmail($to);
+    if($secureCheck == false){
+      echo "Invalid recipient email.";
+    }else{
+      // send email.
+      mail($to, $subject, $message, $headers);
+      echo 'email sent!';
+    }
+
   }
 
-  $to= 'adegboye72@gmail.com';
-  $subject= 'Contact Us Form';
-  $message= 'message sent using php mail function.';
-  $headers= 'From: Contact Us @ laurel-ng.herokuapp.com';
+  require_once "contactForm.php";
 
-  // check if email address is valid or not.
-  $secureCheck= sanitizeEmail($to);
-  if($secureCheck == false){
-    echo "Invalid recipient email.";
-  }else{
-    // send email.
-    mail($to, $subject, $message, $headers);
-    echo 'email sent!';
-  }
 ?>
 
