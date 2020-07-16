c183ba37d0272ce29f062ba4c2edff063102b236
Samson Adegboye <adegboye72@gmail.com>
2020-07-16 06:00:53 +0100

    added and implemented phpmailer.

--- src/app/contact/contact-submit.php
+++ src/app/contact/contact-submit.php
@@ -1,7 +1,45 @@
 
 <?php
 
-  if(isset($_POST['submit'])){
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  
+  require '.\assets\PHPMailer-master\src\Exception.php';
+  require '.\assets\PHPMailer-master\src\PHPMailer.php';
+  require '.\assets\PHPMailer-master\src\SMTP.php';
+
+  $email= new PHPMailer(true);
+
+  // from
+  $email->From= 'example@example.com';
+  $email->FromName= 'mr. example example';
+
+  // to
+  $email->addAddress("adegboye72@gmail.com");
+  
+  // reply to
+  $email->addReplyTo("example@example.com", "reply");
+
+  // cc & bcc
+  $email->addCC("cc@example.com");
+  $email->addBCC("bcc@example.com");
+
+  // send html or plain text
+  $email->isHTML(true);
+
+  $email->Subject= 'subject here.';
+  $email->Body= '<i>message boddy in html here...</i>';
+  $email->AltBody= 'message boddy in plain text here...';
+
+  // send
+  try{
+    $email->send();
+    echo 'Mail sent successfully';
+  }catch(Exception $e){
+    echo 'Mail Not Sent, error: ' . $email->ErrorInfo;
+  }
+
+  /*if(isset($_POST['submit'])){
     function sanitizeEmail($field){
       $field= filter_var($field, FILTER_SANITIZE_EMAIL);
       
@@ -29,7 +67,7 @@
 
   }
 
-  require_once "contactForm.php";
+  require_once "contactForm.php";*/
 
 ?>
 
